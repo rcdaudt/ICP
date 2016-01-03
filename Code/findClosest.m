166c42de80f99d11e25036a0ba16332983924934
Kaisar Kushibar <kukaba.fm@gmail.com>
2016-01-03 08:17:38 +0100

    it's working

--- Code/findClosest.m
+++
@@ -1,9 +0,0 @@
-function index = findClosest(point,data)
-    s = size(data,1);
-    dist = zeros(s,1);
-    for i = 1:s
-        dist(i) = norm(point-data(i,:));
-    end
-    minimum = min(dist);
-    index = find(dist == minimum,1,'first');
-end
